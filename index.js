97625d74ecf739f47e18fd40fa08177553545e17
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-19 15:21:29 -0500

    Request and response objects

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const port = 5000;
 const hostName = "localhost";
 const log = console.log;
 
-app.use(() => {
+app.use((req, res) => {
     log("We got a new request!");
+    res.send("<h1>We got your request. This is the response!</h1>");
 });
 
 app.listen(port, hostName, () => {
