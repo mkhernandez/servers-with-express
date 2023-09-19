bd67ba30e857a1ba6fc6ee3b46967f4b1fa82c7c
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-19 16:03:06 -0500

    express routing basics

--- index.js
+++ index.js
@@ -4,9 +4,30 @@ const port = 5000;
 const hostName = "localhost";
 const log = console.log;
 
-app.use((req, res) => {
-    log("We got a new request!");
-    res.send("<h1>We got your request. This is the response!</h1>");
+// commented out so that it doesn't interfere with our routes
+// app.use((req, res) => {
+//     log("We got a new request!");
+//     res.send("<h1>We got your request. This is the response!</h1>");
+// });
+
+app.get('/', (req, res) => {
+    res.send("This is the home page!");
+});
+
+app.get('/cats', (req, res) => {
+    res.send("MEOW!");
+});
+
+app.post('/cats', (req, res) => {
+    res.send(`POST REQUEST TO CATS! THIS IS A POST REQUEST!`);
+});
+
+app.get('/dogs', (req, res) => {
+    res.send("WOOF!");
+});
+
+app.get('*', (req, res) => {
+    res.send(`I don't know that path!`);
 });
 
 app.listen(port, hostName, () => {
